3db047272e70b94ef7e7a4d7eda1e79378c652da
Pasindu Akalpa <pasinduakalpa1998@gmail.com>
2023-02-01 10:09:29 +0530

    🎨 Completed

--- learning_rust/src/main.rs
+++ learning_rust/src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
-    println!("Hello, world!");
+    // single line comment
+
+    /*
+    Multiline Comment
+     */
+
+    println!("This is println!()");
+    print!("This is print!()");
+
+    println!(
+        "\nThis is how to print non string values {} \nprintln!(\"{{}} \", value)",
+        10
+    );
+
+    println!(
+        "My First Name is {}, my last name is {}",
+        "Pasindu", "Akalpa"
+    );
+
+    println!(
+        "This
+    is Multiline
+    Print Statement"
+    );
+
+    println!("Multiple indexed Placeholders {2} -> {1} -> {0}", 1, 2, 3);
+
+    println!("This is {argument}", argument = "named argument");
+
+    println!("The sum of 10 + 20 = {}", 10 + 20);
 }
