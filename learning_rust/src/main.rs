d7079c2e9a943ed3413e0b5cb4a6052eea3290ca
Pasindu Akalpa <pasinduakalpa1998@gmail.com>
2023-02-05 21:15:07 +0530

    🎨 Completed

--- learning_rust/src/main.rs
+++ learning_rust/src/main.rs
@@ -1,89 +1,86 @@
 fn main() {
     /*
-    Initializing multiple variables
-     */
+    Strings
+       - String slices (&str) - Fixed length strings
+    */
 
-    let (first_number, second_number) = (250, 480.32);
+    let some_string = "Fixed Length String";
+    println!("The text inside the string is \"{}\"", some_string);
+
+    /*
+    Strings
+       - variable length strings
+       - adding removing characters
+       - operations on strings
+       - formatting and concatanating strings
+    */
+
+    let mut growable_string = String::from("This string will grow");
+    println!("The text inside the string is \"{}\"", growable_string);
+
+    growable_string.push('s');
+    println!("Hey the text inside the string is \"{}\"", growable_string);
+
+    growable_string.pop();
+    println!("Hey the text inside the string is \"{}\"", growable_string);
+
+    growable_string.push_str(" which i will use");
+    println!("Hey the text inside the string is \"{}\"", growable_string);
+
     println!(
-        "The first number is {} and the second number is {}",
-        first_number, second_number
+        "I am going to tell you some basic things about the strings, 
+    Is the string empty {},
+    The length of the string is {},
+    The string has {} bytes,
+    Does the string contains the word 'use' {}",
+        growable_string.is_empty(),
+        growable_string.len(),
+        growable_string.capacity(),
+        growable_string.contains("use")
     );
 
-    /*
-    Readability of Large Number
-    */
-
-    let large_number = 1_000_000;
-    println!("The value of the large number is {}", large_number);
-
-    /*
-    Integer overflow
-    */
-
-    // let overflow_number: u8 = 256;
-
-    /*
-    Decimal numbers in other formats
-    */
-
-    let x: i32 = 255;
+    growable_string.push_str("   ");
     println!(
-        "The value of variable x in hexadecimal is {:o} and octal is {:X} and in binary {:b}",
-        x, x, x
+        "the length of the string before the trim is {},
+    length of the string after the trim is {}",
+        growable_string.len(),
+        growable_string.trim().len()
     );
 
-    /*
-    Snake case convention
-    */
+    let number = 6;
+    println!(
+        "The value of the number in string is {}",
+        number.to_string()
+    );
 
-    let Number = 45;
+    println!(
+        "Is the number really a string {}",
+        number.to_string() == "6"
+    );
 
-    /*
-    Opertaion on number in different formats
-    */
+    let some_char = 'a';
+    println!("The character in string is {}", some_char.to_string());
 
-    let n1: i32 = 14;
-    let n2: f64 = 15.6;
-    // let n3 = n1 + n2 as i32;
-    let n3 = n1 as f64 + n2;
+    println!(
+        "Is the character really a string {}",
+        some_char.to_string() == "a"
+    );
 
-    println!("{}", n3);
+    let my_name = "Pasindu Akalpa".to_string();
+    println!("The string contains my name {}", my_name);
 
-    /*
-    Shadowing
-    */
+    let empty_string = String::new();
+    println!("Length is {}", empty_string.len());
 
-    // let s = 5;
-    // let s = 5 * 5;
-    // println!("The value of the variable s = {}", s);
+    let s_1 = "Pasindu".to_string();
+    let s_2 = "Akalpa".to_string();
 
-    let mut s = 5;
-    let s = 5 * 5;
-    println!("The value of the variable s = {}", s);
+    let s_3 = format!("My first name is {} and my last name is {}", s_1, s_2);
+    println!("{}", s_3);
 
-    let s = 32;
-    println!("The value of variable s = {} is currently a integer", s);
+    let string_1 = String::from("Pasindu");
+    let string_2 = String::from("Akalpa");
 
-    let s = 'A';
-    println!("The variable s = {} is currently a char", s);
-
-    let s = 64.5;
-    println!("The variable s = {} is currently a float", s);
-
-    let num = 65;
-    {
-        let num = 60;
-        println!(
-            "The value of the variable s inside the inner scope is {}",
-            num
-        );
-    }
-    println!("The value of the variable s = {}", num);
-
-    /*
-    Constants
-    */
-
-    const MAX_SALARY: u32 = 100_000;
-    println!("The value of the constant is {}", MAX_SALARY);
+    let string_3 = format!("{}{}", string_1, string_2);
+    println!("The concatenated string is {}", string_3);
 }
