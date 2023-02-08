d8ae66b7c9aff1174ba40e75614bbf39b21827b8
Pasindu Akalpa <pasinduakalpa1998@gmail.com>
2023-02-08 20:57:14 +0530

    🎨 Completed

--- learning_rust/src/main.rs
+++ learning_rust/src/main.rs
@@ -1,50 +1,24 @@
 fn main() {
     /*
-    References Rules
-        - One mutable reference in a scope
-        - Many immutable references
-        - mutable and immutable cannot coexist
-        - scope of a reference
-        - data should not change when immutable references are in scope
+    String concatenation and ownership
     */
 
-    // let mut heap_num = vec![4, 5, 6];
-    // let ref1 = &mut heap_num;
-    // let ref2 = &mut heap_num;
-    // println!(
-    //     "The First reference is {:?} and second reference is {:?}",
-    //     ref1, ref2
-    // );
+    // let s1 = String::from("Hello");
+    // let s2: &str = " world";
 
-    // let mut heap_num = vec![4, 5, 6];
-    // let ref1 = &heap_num;
-    // let ref2 = &heap_num;
-    // println!(
-    //     "The First reference is {:?} and second reference is {:?}",
-    //     ref1, ref2
-    // );
+    // let s3 = s1 + s2;
+    // println!("{}", s3);
 
-    // let mut heap_num = vec![4, 5, 6];
-    // let ref1 = &heap_num;
-    // let ref2 = &heap_num;
-    // let ref3 = &mut heap_num;
-    // println!(
-    //     "The First reference is {:?} and second reference is {:?} and third reference is {:?}",
-    //     ref1, ref2, ref3
-    // );
+    // let s1 = String::from("Hello");
+    // let s2 = String::from(" World!");
 
-    // let mut heap_num = vec![4, 5, 6];
-    // let ref1 = &heap_num;
-    // let ref2 = &heap_num;
-    // println!("Immutable references are {:?} and {:?}", ref1, ref2);
+    // let s3 = s1 + &s2;
+    // println!("{} {}", s3, s2);
 
-    // let ref3 = &mut heap_num;
-    // ref3.push(7);
-    // println!("The First reference is {:?}", ref3);
+    let s1 = String::from("Hello");
+    let s2 = String::from("World");
+    let s3 = String::from("From Rust");
 
-    let mut heap_num = vec![4, 5, 6];
-    let ref1 = &heap_num;
-    let ref2 = &heap_num;
-    println!("Immutable references are {:?} and {:?}", ref1, ref2);
-    heap_num.push(86);
+    let s4 = s1 + &s2 + &s3;
+    println!("{} {} {}", s4, s2, s3);
 }
