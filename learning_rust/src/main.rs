5aede97310aab16236e005df2b3a7a25b893f1f8
Pasindu Akalpa <pasinduakalpa1998@gmail.com>
2023-03-06 10:37:56 +0530

    🎨 Completed

--- learning_rust/src/main.rs
+++ learning_rust/src/main.rs
@@ -1,73 +1,43 @@
-use std::io::stdin;
-
-fn new_stack(maxsize: usize) -> Vec<u32> {
-    let vec: Vec<u32> = Vec::with_capacity(maxsize);
+fn new_stack(maxsize: usize) -> Vec<char> {
+    let vec: Vec<char> = Vec::with_capacity(maxsize);
     return vec;
 }
 
-fn pop(stack: &mut Vec<u32>) -> Option<u32> {
+fn pop(stack: &mut Vec<char>) -> Option<char> {
     let poped_value = stack.pop();
-    println!("poped value is {:?}", poped_value);
     return poped_value;
 }
 
-fn push(stack: &mut Vec<u32>, item: u32, maxsize: usize) {
+fn push(stack: &mut Vec<char>, item: char, maxsize: usize) {
     if stack.len() == maxsize {
-        println!("Cannot add more elements")
     } else {
         stack.push(item);
-        println!("Stack: {:?}", stack);
     }
 }
 
-fn size(stack: &Vec<u32>) -> usize {
+fn size(stack: &Vec<char>) -> usize {
     return stack.len();
-}
-
-fn input() -> u32 {
-    let mut n = String::new();
-    stdin().read_line(&mut n).expect("Failed to read input");
-    let n: u32 = n.trim().parse().expect("Invalid input");
-    return n;
 }
 
 fn main() {
     /*
     Stack
        - Stack using vec
+       - Application of stacks (String Reversal)
     */
+    let input_string = String::from("Welcome to Rust");
+    let size_stack = input_string.len();
+    let mut stack = new_stack(size_stack);
+    let mut rev_string = String::new();
 
-    println!("Lets first create a stack for our use");
-    println!("Please mention the size of the stack");
-
-    let size_stack = input();
-    let mut stack = new_stack(size_stack as usize);
-
-    loop {
-        println!("\n\n ***** Menu ***** \n");
-        println!("1. Push \n 2. Pop \n 3. Display \n 4. Size \n 5. Exit");
-        println!("\n Enter your choice: ");
-
-        let choice = input();
-        match choice {
-            1 => {
-                println!("Enter the value to be inserted: ");
-                let item = input();
-                push(&mut stack, item, size_stack as usize);
-            }
-            2 => println!("The element which is poped is {:?}", pop(&mut stack)),
-            3 => println!("The elements are {:?}", stack),
-            4 => println!("The size of the stack is {}", size(&stack)),
-            5 => break,
-            _ => println!("\n Wrong Selection !!! Try Again !!!"),
-        }
-
-        println!("Do you want to continue 1 = Yes / 0 = No");
-        let status = input();
-        if status == 1 {
-            continue;
-        } else {
-            break;
-        }
+    for i in input_string.chars() {
+        push(&mut stack, i, size_stack);
     }
+
+    for i in 0..size(&stack) {
+        rev_string.push(pop(&mut stack).unwrap());
+    }
+
+    println!("The input string is {:?}", input_string);
+    println!("The reverse of the string is {:?}", rev_string);
 }
