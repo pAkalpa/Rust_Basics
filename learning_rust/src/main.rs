61f20a8b16011fcaa8629465524a6ce72f067761
Pasindu Akalpa <pasinduakalpa1998@gmail.com>
2023-03-18 21:21:15 +0530

    🎨 Completed

--- learning_rust/src/main.rs
+++ learning_rust/src/main.rs
@@ -1,43 +1,102 @@
-fn new_stack(maxsize: usize) -> Vec<char> {
-    let vec: Vec<char> = Vec::with_capacity(maxsize);
-    return vec;
-}
-
-fn pop(stack: &mut Vec<char>) -> Option<char> {
-    let poped_value = stack.pop();
-    return poped_value;
-}
-
-fn push(stack: &mut Vec<char>, item: char, maxsize: usize) {
-    if stack.len() == maxsize {
-    } else {
-        stack.push(item);
-    }
-}
-
-fn size(stack: &Vec<char>) -> usize {
-    return stack.len();
-}
-
 fn main() {
     /*
-    Stack
-       - Stack using vec
-       - Application of stacks (String Reversal)
+    Structures
+        - Defining a structure
+        - Tuple Structure
     */
-    let input_string = String::from("Welcome to Rust");
-    let size_stack = input_string.len();
-    let mut stack = new_stack(size_stack);
-    let mut rev_string = String::new();
 
-    for i in input_string.chars() {
-        push(&mut stack, i, size_stack);
+    let person1 = Person {
+        name: String::from("Pasindu Akalpa"),
+        citizenship: String::from("Sri Lanka"),
+        age: 40,
+        gender: 'M',
+        salary: 40_000,
+    };
+
+    println!(
+        "The structure values are {}, {}, {}, {}, {}",
+        person1.name, person1.citizenship, person1.age, person1.gender, person1.salary
+    );
+    println!(
+        "The Taxes on person {} is {}",
+        person1.name,
+        Person::compute_taxes(&person1)
+    );
+
+    let person2 = Person::new();
+    println!("The Person 2 is initialzed with default values");
+    println!(
+        "{} , {} , {} , {} , {}",
+        person2.name, person2.citizenship, person2.age, person2.gender, person2.salary
+    );
+
+    let person3 = Person {
+        age: 50,
+        name: String::from("Example"),
+        ..person1
+    };
+
+    println!(
+        "The name of the person 3 = {} and his salary is {}",
+        person3.name, person3.salary
+    );
+
+    let mut person4 = Person::new();
+    println!("The default name of the person 4 is {}", person4.name);
+    person4.name = String::from("TEST");
+    println!("The update name of the person 4 is {}", person4.name);
+
+    let some_nums = Numbers(32, 16);
+
+    println!(
+        "The values of the two fields are {}, {}",
+        some_nums.0, some_nums.1
+    );
+
+    println!("The greater value is {}", some_nums.greater());
+    println!("The lesser value is {}", some_nums.lesser());
+}
+
+struct Numbers(i32, i32);
+
+impl Numbers {
+    fn greater(&self) -> i32 {
+        if self.0 > self.1 {
+            self.0
+        } else {
+            self.1
+        }
     }
 
-    for i in 0..size(&stack) {
-        rev_string.push(pop(&mut stack).unwrap());
+    fn lesser(&self) -> i32 {
+        if self.0 < self.1 {
+            self.0
+        } else {
+            self.1
+        }
+    }
+}
+
+struct Person {
+    citizenship: String,
+    name: String,
+    age: i32,
+    gender: char,
+    salary: i32,
+}
+
+impl Person {
+    fn new() -> Self {
+        Person {
+            citizenship: String::from("USA"),
+            name: String::from("abc"),
+            age: 40,
+            gender: 'M',
+            salary: 35_000,
+        }
     }
 
-    println!("The input string is {:?}", input_string);
-    println!("The reverse of the string is {:?}", rev_string);
+    fn compute_taxes(&self) -> f32 {
+        (self.salary as f32 / 3.) * 0.5
+    }
 }
