cfb1f6889594b93bd0bf38277da05f9089b58d62
Pasindu Akalpa <pasinduakalpa1998@gmail.com>
2023-04-13 02:08:47 +0530

    🎨 Completed

--- learning_rust/src/main.rs
+++ learning_rust/src/main.rs
@@ -1,37 +1,59 @@
 /*
-   Result Enum
-   - Basic syntax and usage
+   Hash Maps
+   - Defining Hashmap
+   - Inserting Values
+   - Accessing Values
+   - Checking if the value exist
+   - Iterating through elements of hashmap
+   - Updating a value at a specific key
 */
 
-// enum Result<T, E> {
-//     Ok(T),
-//     Err(E),
-// }
+use std::collections::HashMap;
 
-// fn divition(divident: f64, divisor: f64) -> Result<f64, String> {
-//     //  if divisor == 0.0 {
-//     //      Err(String::from("Error: Division by zero"))
-//     //  } else {
-//     //      Ok(divident / divisor)
-//     //  }
-//     match divisor {
-//         0.0 => Err(String::from("Error: Divison by zero")),
-//         _ => Ok(divident / divisor),
+// fn main() {
+//     let mut person: HashMap<&str, i32> = HashMap::new();
+//     person.insert("Pasindu", 25);
+//     person.insert("Jhon", 40);
+//     person.insert("Jane", 30);
+
+//     println!("The age is {:?}", person.get("Jhon").unwrap());
+
+//     if person.contains_key("Pasindu") {
+//         println!("The value exist");
+//     } else {
+//         println!("The value does not exist");
+//     }
+
+//     match person.get("Pasindu") {
+//         Some(Value) => println!("The value exists {}", Value),
+//         None => println!("The value does not exists"),
+//     }
+
+//     for (name, age) in &person {
+//         println!("The person {} has an age of {}", name, age);
 //     }
 // }
 
 // fn main() {
-//     println!("{:?}", divition(9.0, 3.0));
-//     println!("{:?}", divition(4.0, 0.0));
-//     println!("{:?}", divition(0.0, 2.0));
+//     let mut likes: HashMap<&str, &str> = HashMap::new();
+//     //  likes.insert("Pasindu", "games");
+//     //  likes.insert("Pasindu", "apple");
+
+//     //  println!("The things which is being liked is {:?}", likes)
+//     likes.entry("Pasindu").or_insert("apple");
+//     likes.entry("Pasindu").or_insert("asus");
+
+//     println!("The things which is being liked is {:?}", likes)
 // }
 
 fn main() {
-    let some_vec = vec![5, 5, 2, 1, 5, 9];
-    let result = match some_vec.get(6) {
-        Some(a) => Ok(a),
-        None => Err("The value does not exist"),
-    };
+    let some_vec: Vec<i32> = vec![5, 5, 8, 8, 1, 0, 1, 5, 5, 5, 5];
+    let mut freq_vec: HashMap<i32, u32> = HashMap::new();
 
-    println!("The value of result is {:?}", result);
+    for i in &some_vec {
+        let freq: &mut u32 = freq_vec.entry(*i).or_insert(0);
+        *freq += 1;
+    }
+
+    println!("{:?}", freq_vec);
 }
