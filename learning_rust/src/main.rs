84dd1952d6df27cd5ed1d691d0d27c27cd3d6368
Pasindu Akalpa <pasinduakalpa1998@gmail.com>
2023-04-12 12:32:38 +0530

    🎨 Completed

--- learning_rust/src/main.rs
+++ learning_rust/src/main.rs
@@ -1,65 +1,63 @@
 /*
-    Enums
-        - General Syntax
-        - Enums with attached data
-        - Enums to create vector with different types pf data
+   Generics
+   - Motivation (reducing code duplication)
+   - Generics in functions
+   - Generics in structs
 */
 
-// enum Conveyance {
-//     Car(i32),
-//     Train(i32),
-//     Air(i32),
+// fn squarei32(x: i32) -> i32 {
+//     x * x
 // }
 
-// impl Conveyance {
-//     fn travel_allowance(&self) -> f32 {
-//         let allowance = match self {
-//             Conveyance::Car(miles) => *miles as f32 * 14.0 * 2.0,
-//             Conveyance::Train(miles) => *miles as f32 * 18.0 * 2.0,
-//             Conveyance::Air(miles) => *miles as f32 * 30.0 * 2.0,
-//         };
-//         allowance
-//     }
+// fn squaref32(x: f32) -> f32 {
+//     x * x
 // }
 
 // fn main() {
-//     let participant_1 = Conveyance::Car(60);
-//     // println!("The value of the option is {}", participant_1 as i32);
-
-//     let participant_2 = Conveyance::Air(120);
-//     let participant_3 = Conveyance::Train(60);
-
-//     println!(
-//         "The participant 1 has a travel allowance of {}",
-//         participant_1.travel_allowance()
-//     );
-//     println!(
-//         "The participant 2 has a travel allowance of {}",
-//         participant_2.travel_allowance()
-//     );
-//     println!(
-//         "The participant 3 has a travel allowance of {}",
-//         participant_3.travel_allowance()
-//     );
+//     println!("The square of the number is {}", squarei32(5));
+//     println!("The square of the number is {}", squaref32(5.5));
 // }
 
-#[derive(Debug)]
-enum Value {
-    Integer(i32),
-    Float(f32),
+// fn square<T>(x: T) -> T
+// where
+//     T: std::ops::Mul<Output = T> + Copy,
+// {
+//     x * x
+// }
+
+// fn add<T: std::ops::Add<Output = T> + Copy>(x: T) -> T {
+//     x + x
+// }
+
+// fn main() {
+//     println!("The square of the number is {}", square(5));
+//     println!("The square of the number is {}", square(5.5));
+//     println!("The add of the number is {}", add(5.5));
+// }
+
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U>
+where
+    T: std::fmt::Debug,
+    U: std::fmt::Debug,
+{
+    fn printing(&self) {
+        println!("The value of the points are {:?}, {:?}", self.x, self.y);
+    }
 }
 
 fn main() {
-    let some_value = vec![Value::Integer(12), Value::Float(15.5)];
-    println!(
-        "The value of the integer is {:?} and the value of the float is {:?}",
-        some_value[0], some_value[1]
-    );
+    let p1 = Point { x: 5, y: 5 };
 
-    for i in some_value.iter() {
-        match i {
-            Value::Integer(num) => println!("The value is an integer with a value of {}", num),
-            Value::Float(num) => println!("The value is a float with a value of {}", num),
-        }
-    }
+    let p2 = Point { x: 1.0, y: 4.0 };
+
+    let p3 = Point { x: 5, y: 5.0 };
+
+    p1.printing();
+    p2.printing();
+    p3.printing();
 }
