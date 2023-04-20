cd54bd7382224247f2cd32701410af14f09a916f
Pasindu Akalpa <pasinduakalpa1998@gmail.com>
2023-04-20 22:29:47 +0530

    🎨 Completed

--- learning_rust/src/main.rs
+++ learning_rust/src/main.rs
@@ -1,54 +1,63 @@
 /*
-   Function Types
-   - Basic Syntax and Use
-   - Function types as parameter to functions
+   Iterators
+   - Basics of iterators and its syntax
+   - Some useful functions for iterators
+   - Common statistic related functions
+   - Modifying and collecting values
 */
-// fn main() {
-//     let mut f = min;
-//     let mut t = max;
-//     println!("The min of the two value is {}", f(2, 3));
-//     println!("The max of the two value is {}", t(2, 3));
-// }
-
-// fn max(x: i32, y: i32) -> i32 {
-//     if x > y {
-//         x
-//     } else {
-//         y
-//     }
-// }
-
-// fn min(x: i32, y: i32) -> i32 {
-//     if x < y {
-//         x
-//     } else {
-//         y
-//     }
-// }
-
-// fn main() {
-//     let (my_name, my_age) = (String::from("Pasindu"), 25);
-//     prints_full_info(prints_name, &my_name, my_age);
-// }
-
-// fn prints_name(name: &str) {
-//     print!("The name is {}", name);
-// }
-
-// fn prints_full_info(f: fn(&str), some_name: &str, age: i32) {
-//     f(some_name);
-//     println!(" and my age is {}", age)
-// }
-
-fn add_one(x: i32) -> i32 {
-    x + 1
-}
-
-fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
-    f(arg) + f(arg)
-}
-
 fn main() {
-    let answer = do_twice(add_one, 5);
-    println!("The answer is {}", answer);
+    // let some_vec = vec![1, 2, 3, 4, 5, 6, 7];
+    // let mut iter = some_vec.iter();
+    // println!("The iterator : {:?}", iter);
+    // println!("{:?}", iter.next());
+    // println!("{:?}", iter.next());
+    // println!("{:?}", iter.next());
+    // println!("{:?}", iter.next());
+    // println!("{:?}", iter.next());
+    // println!("{:?}", iter.next());
+    // println!("{:?}", iter.next());
+    // println!("{:?}", iter.next());
+
+    // let a: Vec<u32> = vec![0, 1, 2, 4, 5, 6, 9, 8, 7];
+    // let mut check = a.iter().any(|&x| x > 0);
+    // println!("The value of the any function is {}", check);
+
+    // let check = a.iter().all(|&x| x >= 0);
+    // println!("The value of all function is {}", check);
+
+    // let check = a.iter().find(|&&x| x > 0);
+    // println!("The value of function is {}", check.unwrap());
+
+    // let check = a.iter().position(|&x| x > 4);
+    // println!("The value of function position is {}", check.unwrap());
+
+    // let check = a.iter().rposition(|&x| x > 4);
+    // println!("The value of function rposition is {}", check.unwrap());
+
+    // let check = a.iter().max();
+    // println!("The value of function max is {}", check.unwrap());
+
+    // let check = a.iter().min();
+    // println!("The value of function min is {}", check.unwrap());
+
+    // let mut iter = a.iter().rev();
+    // println!("The result of applying rev function : {:?}", iter.next());
+
+    let a = vec![0, 1, 2, 3, 4, 5, 6, 7];
+    let filtered_values = a.iter().filter(|&x| *x >= 5).collect::<Vec<&u32>>();
+    println!("{:?}", filtered_values);
+
+    let b = a.clone();
+    let filtered_values = a.into_iter().filter(|x| *x >= 5).collect::<Vec<u32>>();
+    println!("{:?}", filtered_values);
+
+    let mapped_values = b.iter().map(|x| 2 * *x).collect::<Vec<u32>>();
+    println!("{:?}", mapped_values);
+
+    let mapped_values = b
+        .iter()
+        .map(|x| 2 * *x)
+        .filter(|x| *x > 10)
+        .collect::<Vec<u32>>();
+    println!("{:?}", mapped_values);
 }
