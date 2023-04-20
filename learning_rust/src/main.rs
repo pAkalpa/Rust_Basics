a4d180a4e4a7a5f977535913c7a95ce8b908f4d5
Pasindu Akalpa <pasinduakalpa1998@gmail.com>
2023-04-20 20:51:09 +0530

    🎨 Completed

--- learning_rust/src/main.rs
+++ learning_rust/src/main.rs
@@ -1,77 +1,54 @@
 /*
-    Closures
-    - Basic Syntax
-    - Closure with inputs
-    - Same variable for different closure
-    - Ownership rules and closures
-    - Inference of the output
-    - Passing a closure as a function argument
-    - Borrow by immutable reference
-    - Moving of a value into a closure
+   Function Types
+   - Basic Syntax and Use
+   - Function types as parameter to functions
 */
-
-// |...| {...}
-
 // fn main() {
-//     let x = 5;
-//     let square = |num: i32| println!("The square of {} is {}", num, num * num);
-//     let square = |num: i32| println!("The cube of {} is {}", num, num * num * num);
-//     square(x);
-
-//     let y = 15;
-//     square(y);
+//     let mut f = min;
+//     let mut t = max;
+//     println!("The min of the two value is {}", f(2, 3));
+//     println!("The max of the two value is {}", t(2, 3));
 // }
 
-// fn main() {
-//     let print_user_age = |general_info: String, name: &str, age: i32| {
-//         println!("{} \n\t {} \n\t {}", general_info, name, age)
-//     };
-
-//     let general_info = String::from("The details are");
-//     let (person_name, person_age) = (String::from("Pasindu"), 51);
-
-//     print_user_age(general_info, &person_name, person_age);
-//     println!("The variable has been moved {}", person_name);
-// }
-
-// fn main() {
-//     let square = |num| num * num;
-//     // let x = 5;
-//     // square(x);
-
-//     let y = 5.5;
-//     square(y);
-// }
-
-// fn main() {
-//     let division_status = |y: f32| {
-//         if y != 0.0 {
-//             true
-//         } else {
-//             false
-//         }
-//     };
-//     division(5.0, 10.0, division_status);
-//     division(55.0, 0.0, division_status);
-// }
-
-// fn division<F: Fn(f32) -> bool>(x: f32, y: f32, f: F) {
-//     if f(y) {
-//         println!("The division result is {}", x / y);
+// fn max(x: i32, y: i32) -> i32 {
+//     if x > y {
+//         x
 //     } else {
-//         println!("Division is not possible");
+//         y
 //     }
 // }
 
+// fn min(x: i32, y: i32) -> i32 {
+//     if x < y {
+//         x
+//     } else {
+//         y
+//     }
+// }
+
+// fn main() {
+//     let (my_name, my_age) = (String::from("Pasindu"), 25);
+//     prints_full_info(prints_name, &my_name, my_age);
+// }
+
+// fn prints_name(name: &str) {
+//     print!("The name is {}", name);
+// }
+
+// fn prints_full_info(f: fn(&str), some_name: &str, age: i32) {
+//     f(some_name);
+//     println!(" and my age is {}", age)
+// }
+
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
+    f(arg) + f(arg)
+}
+
 fn main() {
-    let mut vec_1 = vec![1, 2, 3];
-    let mut some_closure = || {
-        let vec_2 = vec_1;
-    };
-
-    // println!("vec 1 {:?}", vec_1);
-
-    some_closure();
-    // println!("vec 1 = {:?}", vec_1);
-    // println!("vec 2 = {:?}", vec_2);
+    let answer = do_twice(add_one, 5);
+    println!("The answer is {}", answer);
 }
