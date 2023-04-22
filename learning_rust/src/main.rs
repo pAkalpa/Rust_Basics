b4f2240d846af93ae37250a9474d6ebb0a793fa7
Pasindu Akalpa <pasinduakalpa1998@gmail.com>
2023-04-22 11:45:50 +0530

    🎨 Completed

--- learning_rust/src/main.rs
+++ learning_rust/src/main.rs
@@ -1,52 +1,18 @@
 /*
-   Modules
+   Using External Crates
 */
-// fn main() {
-//     let rect1 = Rectangle {
-//         length: 5,
-//         width: 10,
-//     };
 
-//     rect_area(&rect1.length, &rect1.width);
-// }
-
-// struct Rectangle {
-//     length: i32,
-//     width: i32,
-// }
-
-// fn some_fn() {
-//     println!("This is the function the main crate");
-// }
-
-// mod maths {
-//     pub mod basic_math {
-//         pub fn multiplication(num1: &i32, num2: &i32) -> i32 {
-//             let result = num1 * num2;
-//             printing(&result);
-//             result
-//         }
-
-//         fn printing(num: &i32) {
-//             println!("The result is {}", num);
-//             crate::some_fn();
-//         }
-//     }
-// }
-
-// pub fn rect_area(length: &i32, width: &i32) -> i32 {
-//     use maths::basic_math::multiplication;
-//     multiplication(length, width)
-// }
-
-// mod file_2;
-
-// fn main() {
-//     file_2::some_person();
-// }
-
-mod file_3;
+use array_tool::vec::*;
 
 fn main() {
-    file_3::allowance();
+    let vec_1 = vec![1, 1, 3, 5, 6, 7];
+    let vec_2 = vec![1, 2, 3];
+
+    let intersection = vec_1.intersect(vec_2.clone());
+    println!("The intersection is {:?}", intersection);
+
+    let union_set = vec_1.union(vec_2.clone());
+    println!("The union is {:?}", union_set);
+
+    println!("Vec 1 three times displayed = {:?}", vec_2.times(3));
 }
